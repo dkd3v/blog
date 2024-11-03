2a8bdc276986c36a8acc5610ae4b59a64fcaf84b
Dxrkd3v <dxrkd3v@vk.com>
2024-11-04 02:24:07 +0300

    Добавлен js для блюра менб при скролле

--- pf/lp4/assets/js/main.js
+++ pf/lp4/assets/js/main.js
@@ -21,12 +21,18 @@ if(navClose){
 
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+/*=============== SWIPER FAVORITES ===============*/
 
 
-/*=============== SWIPER FAVORITES ===============*/ 
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
